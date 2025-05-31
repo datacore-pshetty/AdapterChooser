c07a3c02b91e1ded98ae3f9ce31d56a08bf236b7
Prajwal J <83638742+datacore-pshetty@users.noreply.github.com>
2025-05-31 03:04:18 -0700

    Everything working correctly now

--- ViewModel/ListBoxSelectedItemsBehavior.cs
+++ ViewModel/ListBoxSelectedItemsBehavior.cs
@@ -7,6 +7,8 @@ namespace AdapterChooser.ViewModel
 {
     public class ListBoxSelectedItemsBehavior
     {
+        private static bool _isUpdating = false; // Prevent circular updates
+
         public static readonly DependencyProperty SelectedItemsProperty =
             DependencyProperty.RegisterAttached(
                 "SelectedItems",
@@ -28,38 +30,96 @@ namespace AdapterChooser.ViewModel
         {
             if (d is ListBox listBox)
             {
-                var oldAdapters = e.OldValue as INotifyCollectionChanged;
-                var newAdapters = e.NewValue as INotifyCollectionChanged;
+                // Remove old event handlers
+                listBox.SelectionChanged -= ListBox_SelectionChanged;
 
-                if (oldAdapters != null)
+                var oldCollection = e.OldValue as INotifyCollectionChanged;
+                var newCollection = e.NewValue as INotifyCollectionChanged;
+
+                // Unsubscribe from old collection changes
+                if (oldCollection != null)
                 {
-                    listBox.SelectionChanged -= ListBox_SelectionChanged;
-                }
-                
-                if (newAdapters != null)
-                {
-                    listBox.SelectionChanged += ListBox_SelectionChanged;
+                    oldCollection.CollectionChanged -= Collection_CollectionChanged;
                 }
 
-                //listBox.SelectionChanged -= ListBox_SelectionChanged;
-                //listBox.SelectionChanged += ListBox_SelectionChanged;
+                // Subscribe to new collection changes
+                if (newCollection != null)
+                {
+                    newCollection.CollectionChanged += Collection_CollectionChanged;
+
+                    // Initial sync from collection to ListBox
+                    SyncListBoxFromCollection(listBox, e.NewValue as IList);
+                }
+
+                // Add new event handler
+                listBox.SelectionChanged += ListBox_SelectionChanged;
             }
+        }
+
+        private static void Collection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (_isUpdating) return;
+
+            // Find the ListBox that this collection is bound to
+            var collection = sender as IList;
+            if (collection == null) return;
+
+            // This is a simplified approach - in a real scenario you'd need to track which ListBox this collection belongs to
+            // For now, we'll rely on the SelectionChanged event to handle most updates
         }
 
         private static void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (_isUpdating) return;
+
             if (sender is ListBox listBox)
             {
                 var selectedItems = GetSelectedItems(listBox);
                 if (selectedItems == null) return;
 
-                selectedItems.Clear();
-                foreach (var item in listBox.SelectedItems)
+                _isUpdating = true;
+                try
                 {
-                    selectedItems.Add(item);
-                }
 
-                SetSelectedItems(listBox, selectedItems);
+                    //Make changes in one of the Bridge and then change to Prox-2
+                    //When the host is changed back from Prox-2 to Prox-1 the listBox.SelectedItems will be empty, but the selectedItems will have the old selected values.
+                    //In case the If condition is not there, the selectedItems will be cleared and no items will be added since listBox.SelectedItems is empty.
+                    if (listBox.SelectedItems.Count != 0)
+                    {
+                        // Clear and repopulate the bound collection
+                        selectedItems.Clear();
+                        foreach (var item in listBox.SelectedItems)
+                        {
+                            selectedItems.Add(item);
+                        }
+                    }
+                }
+                finally
+                {
+                    _isUpdating = false;
+                }
+            }
+        }
+
+        private static void SyncListBoxFromCollection(ListBox listBox, IList collection)
+        {
+            if (listBox == null || collection == null || _isUpdating) return;
+
+            _isUpdating = true;
+            try
+            {
+                listBox.SelectedItems.Clear();
+                foreach (var item in collection)
+                {
+                    if (listBox.Items.Contains(item))
+                    {
+                        listBox.SelectedItems.Add(item);
+                    }
+                }
+            }
+            finally
+            {
+                _isUpdating = false;
             }
         }
     }
